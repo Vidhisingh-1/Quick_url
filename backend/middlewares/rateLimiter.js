062c1c24de482bf457af4798145df4bd6297b178
Vidhi Singh <vidhip.ug22.ec@nitp.ac.in>
2025-07-23 18:51:43 +0530

    .

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -3,8 +3,8 @@ const rateLimit=require('express-rate-limit');
 //allows 100 requests per 15 minutes
 
 const limiter=rateLimit({
-    windowMs:15*60*1000,// 15 minutes sec->miliseconds
-    max:3,
+    windowMs:2*60*1000,// 15 minutes sec->miliseconds
+    max:20,
     message:'Too many requests from this IP, please try again after 15 minutes',
     standardHeaders:true,
     legacyHeaders:false,
